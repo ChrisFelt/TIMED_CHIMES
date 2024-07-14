bb160ea47f79dfea9ed884859a71a0dcf38694cc
ChrisFelt <christopher.f.felt@gmail.com>
2024-07-14 03:25:13 -0600

    implemented stacked widget toggling for settings

--- main_window.py
+++ main_window.py
@@ -19,3 +19,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.StackedWidget.addWidget(self._settings)
 
         # slots for toggling between stacked widgets
+        self._display.open_settings_button.clicked.connect(self.open_settings)
+        self._settings.close_settings_button.clicked.connect(self.close_settings)
+
+    def open_settings(self):
+        self.StackedWidget.setCurrentIndex(1)
+    
+    def close_settings(self):
+        self.StackedWidget.setCurrentIndex(0)
