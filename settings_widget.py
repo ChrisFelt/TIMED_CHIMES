43852baecfc229f1ff2c55f4a76759fdd1fa8432
ChrisFelt <christopher.f.felt@gmail.com>
2024-07-17 02:09:51 -0600

    list widget now populates with mp3 files in /sounds

--- settings_widget.py
+++ settings_widget.py
@@ -1,8 +1,20 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import QDir
+from PySide6.QtWidgets import QWidget, QFileSystemModel
 from ui_settings_widget import Ui_SettingsWidget
+import os
 
 
 class SettingsWidget(QWidget, Ui_SettingsWidget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # find absolute path of /sounds
+        wd = QDir.current()
+        path = QDir(wd.path() + "/sounds")
+
+        # list of mp3 files in path
+        sounds_list = path.entryList("*.mp3", QDir.Files)
+
+        # populate list widget
+        self.sounds_list_view.addItems(sounds_list)
